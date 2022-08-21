9bef0cafed6a30ed6f4261020defc5762025feed
crmorris <59937438+crmorris@users.noreply.github.com>
2022-08-20 22:12:40 -0500

    Add files via upload

---
+++ HousingData.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.ML.Data;
+
+//This is where you define the schema for your data
+internal class HousingData
+{
+    //LoadColumn loads the respective column from the dataset
+    [LoadColumn(0)]
+    public float Longitude { get; set; }
+
+    [LoadColumn(1)]
+    public float Latitude { get; set; }
+
+    [LoadColumn(2)]
+    public float HousingMedianAge { get; set; }
+
+    [LoadColumn(3)]
+    public float TotalRooms { get; set; }
+
+    [LoadColumn(4)]
+    public float TotalBedrooms { get; set; }
+
+    [LoadColumn(5)]
+    public float Population { get; set; }
+
+    [LoadColumn(6)]
+    public float Households { get; set; }
+
+    [LoadColumn(7)]
+    public float MedianIncome { get; set; }
+
+    //Because this is the data we want to predict, we also give it the attribute of "ColumnName"
+    [LoadColumn(8), ColumnName("Label")]
+    public float MedianHouseValue { get; set; }
+
+    [LoadColumn(9)]
+    public string OceanProximity { get; set; }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.ML;
+
+//We need an instance of the context to use ML.NET
+var context = new MLContext();
+
+//Loads in our data
+var data = context.Data.LoadFromTextFile<HousingData>("./housing.csv", hasHeader: true, separatorChar: ',');
+
+//We are splitting our data here, so we can teach the model with one section and test the model on another section
+var split = context.Data.TrainTestSplit(data, testFraction: 0.2);
+
+//"These are the columns we give our algorithm to allow it to use the data"
+//Label is filtered out here because it's our target column; OceanProximity is filtered out because it's a string
+//and every other column is a float
+var features = split.TrainSet.Schema
+    .Select(col => col.Name)
+    .Where(colName => colName != "Label" && colName != "OceanProximity")
+    .ToArray();
+
+//Pre-processing data and choosing ML algorithm
+//"FeaturizeText" normalizes the string values
+var pipeline = context.Transforms.Text.FeaturizeText("Text", "OceanProximity")
+    //For the pipeline, just append as many things as necessary
+    //This line concatenates the feature query above and adds it to the "Features" column
+    .Append(context.Transforms.Concatenate("Features", features))
+    .Append(context.Transforms.Concatenate("Feature", "Features", "Text"))
+    //This selects the regression method
+    .Append(context.Regression.Trainers.LbfgsPoissonRegression());
+
+//This trains the model
+var model = pipeline.Fit(split.TrainSet);
+
+//Uses the newly trained model to make predictions
+var predictions = model.Transform(split.TestSet);
+
+//Evaluates the accuracy of the predictions
+var metrics = context.Regression.Evaluate(predictions);
+
+Console.WriteLine($"R^2 - {metrics.RSquared}");
+
+//Summary - creates context, loads data, splits data, adds column, pre-processes/chooses algorithm,
+//trains the model, tests against predictions, prints results
+
+//Also important to know - add model builder extension, change CSV property to "copy if newer"
